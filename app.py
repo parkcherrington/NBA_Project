d99f1c4282673d87bec3583d15e3cd3445ab0cf8
Parker Cherrington <parker.cherrington@gmail.com>
2022-11-19 00:08:19 -0700

    Updated EDA script

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
 from shiny import App, render, ui
 import pandas as pd
 import matplotlib.pyplot as plt
+# from shinywidgets import output_widget, register_widget
+import plotly.express as px
+
 
 boxdf = pd.read_csv('boxScores.csv')
 totals = boxdf.groupby(['PLAYER_NAME', 'TEAM_NAME'], as_index = False).sum()
 means = boxdf.groupby(['PLAYER_NAME', 'TEAM_NAME'], as_index = False).mean()
 
 app_ui = ui.page_fluid(
-    ui.input_slider('n', 'Number of Players', 0, 25, 15),
-    ui.input_checkbox('avg', 'Show League Average?'),
-    ui.input_radio_buttons('stat', 'Statistical Category', 
-    {'PTS': "Points", 'REB': "Rebounds", 'AST': "Assists", 'FANTASY': "Fantasy Points"}),
-    ui.output_plot("plot"),
+    ui.navset_pill(
+        ui.nav('Per game leaders', 
+            ui.layout_sidebar(
+                ui.panel_sidebar(
+                    ui.input_slider('n', 'Number of Players', 0, 25, 15),
+                    ui.input_checkbox('avg', 'Show League Average?'),
+                    ui.input_radio_buttons('stat', 'Statistical Category',
+                        {'PTS': "Points", 'REB': "Rebounds", 'AST': "Assists", 'FANTASY': "Fantasy Points"}),
+                ),
+                ui.panel_main(
+                    ui.output_plot("plot"))
+            )
+        ), 
+        ui.nav('Player Lookup', 
+            ui.input_text('name', 'Player Name', placeholder = 'Enter player name here')
+        )
+    )
 )
 
 def server(input, output, session):
@@ -23,11 +38,10 @@ def server(input, output, session):
         data = means[input.stat()].sort_values(ascending = False).head(input.n())
         plt.title(f'NBA Top {input.n()} {input.stat()} Leaders Per Game')
         plt.xlabel(input.stat())
-        plt.xlabel('Player Name')
+        plt.ylabel('Player Name')
         ax.barh(means.iloc[data.index]['PLAYER_NAME'], data)
         if input.avg():
             plt.axvline(means[input.stat()].mean(), color = 'red')
         return fig
     
-
 app = App(app_ui, server, debug = True)
